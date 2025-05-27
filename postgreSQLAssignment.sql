4bc3139501b20f00cc2db6f7af5e7bffdffffbf3
Anamul Hassan <99325446+itsanamulhassan@users.noreply.github.com>
2025-05-27 19:42:08 +0600

    update: constrain

--- postgreSQLAssignment.sql
+++ postgreSQLAssignment.sql
@@ -4,24 +4,24 @@
 
 CREATE TABLE rangers (
     ranger_id SERIAL PRIMARY KEY,
-    "name" VARCHAR(100),
-    region VARCHAR(100)
+    "name" VARCHAR(100) NOT NULL,
+    region VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE species (
     species_id SERIAL PRIMARY KEY,
-    common_name VARCHAR(100),
-    scientific_name VARCHAR(100),
-    discovery_date DATE,
-    conservation_status VARCHAR(100)
+    common_name VARCHAR(100) NOT NULL,
+    scientific_name VARCHAR(100) NOT NULL,
+    discovery_date DATE NOT NULL,
+    conservation_status VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE sightings (
     sighting_id SERIAL PRIMARY KEY,
     ranger_id INTEGER REFERENCES rangers(ranger_id),
     species_id INTEGER REFERENCES species(species_id),
-    sighting_time TIMESTAMP,
-    "location" VARCHAR(200),
+    sighting_time TIMESTAMP NOT NULL,
+    "location" VARCHAR(200) NOT NULL,
     notes TEXT
 );
 
